a39e9640103be0e796e8773f9a930773c312789d
Himanshu Chaurasiya <himanshu.chaurasiya@nucleusteq.com>
2023-05-15 12:44:33 +0530

    payment service completed

---
+++ payment/src/main/java/com/ecommerce/payment/dto/AddWalletInDto.java
@@ -0,0 +1,37 @@
+package com.ecommerce.payment.dto;
+
+import java.util.Objects;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class AddWalletInDto {
+
+    private String userId;
+    private String cardNo;
+    private double balance;
+    private Boolean isDefault;
+	@Override
+	public int hashCode() {
+		return Objects.hash(balance, cardNo, isDefault, userId);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof AddWalletInDto)) {
+			return false;
+		}
+		AddWalletInDto other = (AddWalletInDto) obj;
+		return Double.doubleToLongBits(balance) == Double.doubleToLongBits(other.balance)
+				&& Objects.equals(cardNo, other.cardNo) && Objects.equals(isDefault, other.isDefault)
+				&& Objects.equals(userId, other.userId);
+	}
+    
+    
+}
---
+++ payment/src/main/java/com/ecommerce/payment/dto/WalletOutDto.java
@@ -0,0 +1,38 @@
+package com.ecommerce.payment.dto;
+
+import java.util.Objects;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class WalletOutDto {
+
+	private String id;
+    private String userId;
+    private String cardNo;
+    private double balance;
+    private Boolean isDefault;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(balance, cardNo, id, isDefault, userId);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof WalletOutDto)) {
+			return false;
+		}
+		WalletOutDto other = (WalletOutDto) obj;
+		return Double.doubleToLongBits(balance) == Double.doubleToLongBits(other.balance)
+				&& Objects.equals(cardNo, other.cardNo) && Objects.equals(id, other.id) && isDefault == other.isDefault
+				&& Objects.equals(userId, other.userId);
+	}
+	
+}
